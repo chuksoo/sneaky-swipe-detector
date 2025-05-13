4f94230e56d88e84b16947814403fdde1ac576e3
chuksoo <mexx4me@yahoo.com>
2025-05-13 03:29:01 -0800

    Create function to download data

--- scripts/data_download.py
+++ scripts/data_download.py
@@ -1,10 +1,9 @@
 import pandas as pd
 
-
 def get_data(url):
     try:
         data = pd.read_json(url, compression='zip', lines=True)
-        data.to_parquet('../data/raw/transactions.parquet", engine="fastparquet')
+        data.to_parquet('../data/raw/transactions.parquet', engine='fastparquet')
     except:
         data = pd.read_parquet('../data/raw/sweaky-swipe-transactions.parquet')
     return data
